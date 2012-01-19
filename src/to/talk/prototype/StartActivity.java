f4fc4aabeacd956145c3be98d8312741278d171a
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-01-19 18:44:54 +0530

    (task 818) project created for ics prototyping

--- src/to/talk/prototype/StartActivity.java
+++ src/to/talk/prototype/StartActivity.java
@@ -12,6 +12,6 @@ public class StartActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start);
+        setContentView(R.layout.main);
     }
 }
