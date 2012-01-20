eefadfb41b9aaebb6668a66954d206ad08808c4c
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-01-20 16:36:26 +0530

    (task 818) added drawable folder

--- src/to/talk/prototype/MainActivity.java
+++ src/to/talk/prototype/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 
 
-public class StartActivity extends Activity
+public class MainActivity extends Activity
 {
     /** Called when the activity is first created. */
     @Override
