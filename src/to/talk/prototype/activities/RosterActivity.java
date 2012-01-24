aada768a8dc27e08e58f3879318fcd93ffeac592
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-01-24 14:16:45 +0530

    (task 818) roster entry for ics added

--- src/to/talk/prototype/activities/RosterActivity.java
+++ src/to/talk/prototype/activities/RosterActivity.java
@@ -2,9 +2,7 @@ package to.talk.prototype.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Spinner;
 import to.talk.prototype.R;
 import to.talk.prototype.adapters.RosterEntryAdapter;
 
@@ -12,7 +10,6 @@ public final class RosterActivity extends Activity
 {
 
     private ListView listView;
-    private Spinner spinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -21,35 +18,11 @@ public final class RosterActivity extends Activity
         setContentView(R.layout.roster_list);
 
         listView = (ListView) findViewById(R.id.rosterList);
-        spinner = (Spinner)  getLayoutInflater().inflate(R.layout.roster_entry,null).findViewById(R.id.spinner);
-
-        //populateSpinner();
-
-        populateContactList();
-
-
-    }
-
-    private void populateContactList()
-    {
-        String[] values = new String[] { "Pawan Maheshwari", "Aditya", "Kashif"};
-
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
-                R.layout.roster_entry,R.id.contactEntryText, values);
 
         RosterEntryAdapter rosterEntryAdapter = new RosterEntryAdapter(this);
+
         listView.setAdapter(rosterEntryAdapter);
+
+
     }
-
-    private void populateSpinner()
-    {
-        String[] values = new String[] { "Busy", "Offline", "Invisible"};
-
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,values);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-        spinner.setAdapter(spinnerAdapter);
-    }
-
-
 }
--- src/to/talk/prototype/adapters/RosterEntryAdapter.java
+++ src/to/talk/prototype/adapters/RosterEntryAdapter.java
@@ -2,7 +2,6 @@ package to.talk.prototype.adapters;
 
 import android.app.Activity;
 import android.database.DataSetObserver;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -21,17 +20,11 @@ import to.talk.prototype.R;
 public class RosterEntryAdapter extends BaseAdapter
 {
     private Activity mContext;
-    private String[] Values = new String[] { "Pawan Maheshwari", "Aditya", "Kashif"};
+    private String[] mContactList = new String[] { "Pawan Maheshwari", "Aditya", "Kashif"};
 
     public RosterEntryAdapter(Activity context)
     {
        mContext=context;
-    }
-
-    static class ViewHolder
-    {
-        protected TextView text;
-        protected Spinner spin;
     }
 
     public void registerDataSetObserver(DataSetObserver dataSetObserver)
@@ -46,12 +39,12 @@ public class RosterEntryAdapter extends BaseAdapter
 
     public int getCount()
     {
-        return Values.length;  //To change body of implemented methods use File | Settings | File Templates.
+        return mContactList.length;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     public Object getItem(int i)
     {
-        return Values[i];  //To change body of implemented methods use File | Settings | File Templates.
+        return mContactList[i];  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     public long getItemId(int i)
@@ -68,10 +61,10 @@ public class RosterEntryAdapter extends BaseAdapter
     {
         View view = null;
 
-        if (convertView == null) {
+        if (convertView == null)
+        {
 
-            LayoutInflater inflator = mContext.getLayoutInflater();
-            view = inflator.inflate(R.layout.roster_entry, null);
+            view = mContext.getLayoutInflater().inflate(R.layout.roster_entry, null);
 
             final ViewHolder viewHolder = new ViewHolder();
             viewHolder.text = (TextView) view.findViewById(R.id.contactEntryText);
@@ -84,23 +77,18 @@ public class RosterEntryAdapter extends BaseAdapter
 
             viewHolder.spin.setAdapter(spinnerAdapter);
 
-            // Used to handle events when the user changes the Spinner selection:
-
-            // Update the TextView to reflect what's in the Spinner
-            viewHolder.text.setText(Values[position]);
+            viewHolder.text.setText(mContactList[position]);
 
             view.setTag(viewHolder);
 
 
-        } else {
+        } else
+        {
             view = convertView;
         }
 
         ViewHolder holder = (ViewHolder) view.getTag();
         holder.text.setText(getItem(position).toString());
-        //holder.spin.setSelection(getItem(position).getSelected());
-
-
         return view;
     }
 
@@ -117,5 +105,11 @@ public class RosterEntryAdapter extends BaseAdapter
     public boolean isEmpty()
     {
         return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    static class ViewHolder
+    {
+        protected TextView text;
+        protected Spinner spin;
     }
 }
