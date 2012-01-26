1152ca3625a6023b9ac511e6e73514cb227c9669
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-01-26 14:22:41 +0530

    (task 835) contact roster added

--- gen/to/talk/prototype/R.java
+++ gen/to/talk/prototype/R.java
@@ -225,15 +225,25 @@ public final class R {
     public static final class id {
         public static final int accountList=0x7f080002;
         public static final int accountName=0x7f080000;
-        public static final int menu_refresh=0x7f080003;
-        public static final int menu_search=0x7f080004;
-        public static final int menu_share=0x7f080005;
+        public static final int avatarFrameLayout=0x7f080007;
+        public static final int contactAvatar=0x7f080008;
+        public static final int menu_refresh=0x7f08000d;
+        public static final int menu_search=0x7f08000b;
+        public static final int menu_share=0x7f08000c;
+        public static final int name=0x7f080005;
+        public static final int name_presence=0x7f080003;
+        public static final int presence=0x7f080004;
         public static final int rosterEntrySpinner=0x7f080001;
+        public static final int rosterList=0x7f08000a;
+        public static final int serviceType=0x7f080009;
+        public static final int status=0x7f080006;
     }
     public static final class layout {
         public static final int account_item=0x7f030000;
         public static final int account_list=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int roster_item=0x7f030003;
+        public static final int roster_list=0x7f030004;
     }
     public static final class menu {
         public static final int action_bar=0x7f070000;
---
+++ src/to/talk/prototype/entities/RosterContact.java
@@ -0,0 +1,94 @@
+package to.talk.prototype.entities;
+
+
+public class RosterContact
+{
+    private String mFirstName;
+    private String mLastName;
+    private String mNickName;
+    private String mJid;
+    private String mStatus;
+    private String mPresence;
+    private String mAvatar;
+    private String mServiceType;
+
+    public String getmFirstName()
+    {
+        return mFirstName;
+    }
+
+    public void setmFirstName(String mFirstName)
+    {
+        this.mFirstName = mFirstName;
+    }
+
+    public String getmLastName()
+    {
+        return mLastName;
+    }
+
+    public void setmLastName(String mLastName)
+    {
+        this.mLastName = mLastName;
+    }
+
+    public String getmNickName()
+    {
+        return mNickName;
+    }
+
+    public void setmNickName(String mNickName)
+    {
+        this.mNickName = mNickName;
+    }
+
+    public String getmJid()
+    {
+        return mJid;
+    }
+
+    public void setmJid(String mJid)
+    {
+        this.mJid = mJid;
+    }
+
+    public String getmStatus()
+    {
+        return mStatus;
+    }
+
+    public void setmStatus(String mStatus)
+    {
+        this.mStatus = mStatus;
+    }
+
+    public String getmPresence()
+    {
+        return mPresence;
+    }
+
+    public void setmPresence(String mPresence)
+    {
+        this.mPresence = mPresence;
+    }
+
+    public String getmAvatar()
+    {
+        return mAvatar;
+    }
+
+    public void setmAvatar(String mAvatar)
+    {
+        this.mAvatar = mAvatar;
+    }
+
+    public String getmServiceType()
+    {
+        return mServiceType;
+    }
+
+    public void setmServiceType(String mServiceType)
+    {
+        this.mServiceType = mServiceType;
+    }
+}
