033d476b37a5bf4777a720a926c061387924947c
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-02-14 20:45:24 +0530

    (task 923) backward compatibility issues

--- src/to/talk/prototype/activities/ContactsFragmentActivity.java
+++ src/to/talk/prototype/activities/ContactsFragmentActivity.java
@@ -2,6 +2,7 @@ package to.talk.prototype.activities;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,24 +15,33 @@ import to.talk.prototype.listeners.ActionBarTabListener;
 public final class ContactsFragmentActivity extends ActionBarActivity
 {
     ActionBar actionBar;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.addTab(createTab("Active Chats","activeChats",ActiveChatsFragment.class));
-        actionBar.addTab(createTab("Contacts","allContacts",AllContactsFragment.class));
+        final boolean ICS = Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+
+        if (ICS)
+        {
+            actionBar = getActionBar();
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.addTab(createTab("Active Chats", "activeChats", ActiveChatsFragment.class));
+            actionBar.addTab(createTab("Contacts", "allContacts", AllContactsFragment.class));
+        }
+        else
+        {
+
+        }
     }
 
     private ActionBar.Tab createTab(String text, String tag, Class clz)
     {
         ActionBar.Tab tab = actionBar.newTab();
         tab.setText(text);
-        tab.setTabListener(new ActionBarTabListener(this,tag,clz));
+        tab.setTabListener(new ActionBarTabListener(this, tag, clz));
         return tab;
     }
 
@@ -59,7 +69,7 @@ public final class ContactsFragmentActivity extends ActionBarActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        MenuInflater menuInflater =  getMenuInflater() ;
+        MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.action_bar_contacts, menu);
         return super.onCreateOptionsMenu(menu);
     }
